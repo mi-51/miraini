1021853142e3a2507e4ef99ee904db41640108a2
mizuki <75738412+mizuki51@users.noreply.github.com>
2021-12-04 01:01:05 +0900

    first commit 1203

---
+++ js/script.js
@@ -0,0 +1,27 @@
+$(function () {
+  $('.p-tabmenu__item').on('click', function () {
+    $('.selected').removeClass('selected');
+    $(this).addClass('selected');
+    $('.current').removeClass('current');
+    const index = $(this).index();
+    $('.p-infoList__unit').eq(index).addClass('current');
+  });
+});
+
+$(function () {
+  $('a[href^="#"]').click(function () {
+    let w = $(window).width();
+    let headerHight;
+    if (w > 1146 ) {
+      headerHight = 130;
+    } else {
+      headerHight = 30;
+    }
+    let speed = 550;
+    let href = $(this).attr("href");
+    let target = $(href == "#" || href == "" ? 'html' : href);
+    let position = target.offset().top - headerHight;
+    $('body,html').animate({ scrollTop: position }, speed, 'swing');
+    return false;
+  });
+});
